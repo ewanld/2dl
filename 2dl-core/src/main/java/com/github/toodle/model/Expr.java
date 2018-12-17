dc78647ae339cdbeff848cccfdb820d54cf48a7d
Ewan LASSUDRIE-DUCHESNE <ewan.lassudrieduchesne@atos.net>
2018-12-17 11:34:10 +0100

    add suport for array expressions

--- 2dl-core/src/main/java/com/github/toodle/model/Expr.java
+++ 2dl-core/src/main/java/com/github/toodle/model/Expr.java
@@ -1,9 +1,12 @@
 package com.github.toodle.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Expr {
+	// Possible types: BigDecimal, String, Var, List
 	private final Object value;
 
 	public Expr(String value) {
@@ -21,17 +24,43 @@ public class Expr {
 		this.value = value;
 	}
 
+	public Expr(List<Expr> value) {
+		Objects.requireNonNull(value);
+		this.value = value;
+	}
+
 	public String toLiteral() {
-		if (value instanceof BigDecimal) {
-			return ((BigDecimal) value).toPlainString();
-		} else if (value instanceof String) {
+		if (isBigDecimal()) {
+			return getAsBigDecimal().toPlainString();
+		} else if (isString()) {
 			// TODO escape
-			return "\"" + (String) value + "\"";
-		} else if (value instanceof Var) {
+			return "\"" + getAsString() + "\"";
+		} else if (isVar()) {
 			return "$" + getAsVar().getName();
+		} else if (isList()) {
+			return "[" + getAsList().stream().map(Expr::toLiteral).collect(Collectors.joining(" ")) + "]";
 		} else {
 			throw new IllegalArgumentException("Unknown type: " + value.getClass());
 		}
+	}
+
+	public boolean isList() {
+		return value instanceof List;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Expr> getAsList() {
+		if (value instanceof List) {
+			return (List<Expr>) value;
+		} else {
+			throw new RuntimeException(String.format("Expression '%s' of type %s cannot be cast to List", value,
+					value.getClass().getSimpleName()));
+		}
+
+	}
+
+	public boolean isString() {
+		return value instanceof String;
 	}
 
 	public String getAsString() {
@@ -45,6 +74,15 @@ public class Expr {
 
 	public Object getAsObject() {
 		return value;
+	}
+
+	public boolean isBoolean() {
+		try {
+			getAsBoolean();
+			return true;
+		} catch (final RuntimeException e) {
+			return false;
+		}
 	}
 
 	public boolean getAsBoolean() {
@@ -75,6 +113,15 @@ public class Expr {
 		}
 	}
 
+	public boolean isInt() {
+		try {
+			getAsInt();
+			return true;
+		} catch (final ArithmeticException e) {
+			return false;
+		}
+	}
+
 	public int getAsInt() {
 		return getAsBigDecimal().intValueExact();
 	}
@@ -83,12 +130,35 @@ public class Expr {
 		return getAsBigDecimal().longValueExact();
 	}
 
+	public boolean isPrimitive() {
+		return isBigDecimal() || isString() || isVar();
+	}
+
+	public boolean isBigDecimal() {
+		return value instanceof BigDecimal;
+	}
+
 	public BigDecimal getAsBigDecimal() {
 		if (value instanceof BigDecimal) {
 			return ((BigDecimal) value);
 		} else {
 			throw new RuntimeException(String.format("Expression '%s' of type %s cannot be cast to BigDecimal", value,
 					value.getClass().getSimpleName()));
+		}
+	}
+
+	@Override
+	public String toString() {
+		return toLiteral();
+	}
+
+	public DataType getType() {
+		if (isString()) {
+			return new DataType(DataTypeEnv.TYPE_STRING);
+		} else if (isBigDecimal()) {
+			return new DataType(DataTypeEnv.TYPE_NUMBER);
+		} else {
+			return null;
 		}
 	}
 }
